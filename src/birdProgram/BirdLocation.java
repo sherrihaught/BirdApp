67fdcfc364095f8a2d79b6dbebd9cfb5f1fc2b3b
wilsonluu <wilsonluu@hotmail.com>
2012-11-18 17:08:26 -0800

    Update src/birdProgram/BirdLocation.java
    updated class for BirdAttribute

--- src/birdProgram/BirdLocation.java
+++ src/birdProgram/BirdLocation.java
@@ -1,40 +1,16 @@
 package birdProgram;
 
-public class BirdLocation 
+
+public class BirdLocation extends BirdAttribute
 {
 
-	private String myLocation;
-	
-	public BirdLocation()
-	{
+	public BirdLocation(){
 		myLocation = "";
 	}
 	
-	public BirdLocation( String location)
-	{
+	public BirdLocation( String location){
 		myLocation = location;
 	}
 
-	public String getMyLocation() {
-		return myLocation;
-	}
-
-	public void setMyLocation(String myLocation) {
-		this.myLocation = myLocation;
-	}
 	
-	public int compareTo(BirdLocation location){
-		return myLocation.compareTo(location.getMyLocation());
-	}
-	
-	public boolean equals(Object obj){
-		if(myLocation.equals(((BirdLocation)obj).getMyLocation()) && this.getClass().equals(obj.getClass()))
-			return true;
-		else
-			return false;
-	}
-	
-	public String toString(){
-		return myLocation;
-	}
 }
