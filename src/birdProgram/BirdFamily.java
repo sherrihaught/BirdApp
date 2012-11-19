621e83aad3f7bd2b90b288e018236900454b616c
wilsonluu <wilsonluu@hotmail.com>
2012-11-18 17:07:26 -0800

    Update src/birdProgram/BirdFamily.java
    updated class for BirdAttribute

--- src/birdProgram/BirdFamily.java
+++ src/birdProgram/BirdFamily.java
@@ -1,39 +1,16 @@
 package birdProgram;
 
-public class BirdFamily 
+
+public class BirdFamily extends BirdAttribute
 {
 
-	private String myFamily;
-	
-	public BirdFamily()
-	{
+	public BirdFamily(){
 		myFamily = "";
 	}
-	public BirdFamily(String family)
-	{
+	
+	public BirdFamily(String family){
 		myFamily = family;
 	}
 
-	public String getMyFamily() {
-		return myFamily;
-	}
-
-	public void setMyFamily(String myFamily) {
-		this.myFamily = myFamily;
-	}
 	
-	public int compareTo(BirdFamily family){
-		return myFamily.compareTo(family.getMyFamily());
-	}
-	
-	public boolean equals(Object obj){
-		if(myFamily.equals(((BirdFamily)obj).getMyFamily()) && this.getClass().equals(obj.getClass()))
-			return true;
-		else
-			return false;
-	}
-	
-	public String toString(){
-		return myFamily;
-	}
 }
